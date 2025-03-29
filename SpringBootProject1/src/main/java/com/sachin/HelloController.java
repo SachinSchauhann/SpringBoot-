99b79507c1a2f398ab371ec04a05bc1a4d0ba231
SachinSchauhann <sachins9598@gmail.com>
2025-03-29 20:26:04 +0530

    spring boot all

---
+++ SpringBootProject1/src/main/java/com/sachin/HelloController.java
@@ -0,0 +1,24 @@
+//package com.sachin;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//@Controller
+//public class HelloController {
+//	@RequestMapping("/p7")
+////public String OpenF2(Model model ) {
+//	public ModelAndView OpenF2() {	
+//	ModelAndView mv = new ModelAndView();
+//	String name="Sachin Singh Chauhan";
+//	//String add="  Basti Uttar Pradesh";
+////	model.addAttribute("n",name);
+////	model.addAttribute("m",add);
+//	((Model) mv).addAttribute("n",name);
+//	
+//	//mv.addAttribute("m",add);
+//	mv.setViewName("f2");
+//	//return "f2";
+//	return mv;
+//}
+//}
---
+++ ThymeleafDemo-1/src/main/java/com/sachin/MyController.java
@@ -0,0 +1,25 @@
+package com.sachin;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+    @RequestMapping("/Home")
+    public String openHome(Model m) {
+    	User u=new User();
+    	u.setName("Sachin");
+    	u.setAge(23);
+    	m.addAttribute("user", u);
+    	return "Home";
+    }
+    @RequestMapping("/save")
+	public String processHome(@ModelAttribute User user, Model m) {
+    	
+    	m.addAttribute("msg", "Welcome : "+user.getName()+",  And Your Age : "+user.getAge());
+		return "s";
+	}
+	
+}
+
