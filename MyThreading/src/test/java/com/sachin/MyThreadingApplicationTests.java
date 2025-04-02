d74a9b8aa71bc508365c6c2c2d04b04bcb378e9b
SachinSchauhann <sachins9598@gmail.com>
2025-04-02 20:05:48 +0530

    threding

---
+++ MyThreading/src/test/java/com/sachin/MyThreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyThreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
